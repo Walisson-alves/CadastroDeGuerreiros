7953d52a928d6211ab60db7a779248409b255584
walisson alves pereira <walisson.fw1@gmail.com>
2026-05-12 21:02:20 -0300

    primiro commit

---
+++ src/test/java/com/java87/CadastroDeGuerreiros/CadastroDeGuerreirosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java87.CadastroDeGuerreiros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeGuerreirosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

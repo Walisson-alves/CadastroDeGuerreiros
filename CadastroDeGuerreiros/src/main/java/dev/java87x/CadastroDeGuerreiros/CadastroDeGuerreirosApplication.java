1917f83258cf159c6d6cc615ee1e3780c945dd8f
walisson alves pereira <walisson.fw1@gmail.com>
2026-05-14 18:08:39 -0300

    Arquivos do projeto adicionados com sucesso

---
+++ CadastroDeGuerreiros/src/main/java/dev/java87x/CadastroDeGuerreiros/CadastroDeGuerreirosApplication.java
@@ -0,0 +1,13 @@
+package dev.java87x.CadastroDeGuerreiros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeGuerreirosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeGuerreirosApplication.class, args);
+	}
+
+}
